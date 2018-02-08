732f1d8ab1075d8ae6dd8cb4ec973ad1303269a0
Leticia <DE554GY@SA.EY.NET>
2018-02-08 17:08:06 -0600

    Tarea variables y estructuras

---
+++ INTRODUCCION/Variables_estructuras_de_control.js
@@ -0,0 +1,15 @@
+var mes = 10;
+var informacion = "carne: 18010346 Nombre:Leticia Beatriz Mejía Agustín";
+if (mes==12 || mes==1 || mes==2) {
+  alert("es invierno")
+} else if (mes==3 ||mes==5) {
+  alert("es primavera")
+
+} else if (mes==6 || mes==8) {
+  alert("es verano")
+
+} else if (mes==9 || mes==11){
+  alert("es otoño")
+
+}
+alert(informacion)
